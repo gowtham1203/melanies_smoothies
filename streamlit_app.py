10ca4b4b61e1319e98c12afbffccc129cc48684d
T M GOWTHAM <84575594+gowtham1203@users.noreply.github.com>
2025-06-09 14:14:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredients_list:
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
   
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
